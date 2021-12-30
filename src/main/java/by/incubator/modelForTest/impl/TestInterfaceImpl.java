42269455c16c92578589119f0766fa84bd7e094c
Anton Starasotnikau <anton.starasotnikau@gmail.com>
2021-12-30 13:39:24 +0300

    Create interface and class for test
    Class TestInterfaceImpl implement TestInterface and have empty constructor without parameters and marked Inject annotation

---
+++ src/main/java/by/incubator/modelForTest/impl/TestInterfaceImpl.java
@@ -0,0 +1,10 @@
+package by.incubator.modelForTest.impl;
+
+import by.incubator.infrastructur.annotation.Inject;
+import by.incubator.modelForTest.TestInterface;
+
+public class TestInterfaceImpl implements TestInterface {
+    @Inject
+    public TestInterfaceImpl() {
+    }
+}
