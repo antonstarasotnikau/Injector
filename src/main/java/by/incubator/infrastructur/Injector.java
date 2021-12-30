f27f969a28b1715556049faa19df055afd7ba2f0
Anton Starasotnikau <anton.starasotnikau@gmail.com>
2021-12-30 12:46:54 +0300

    Create interface Injector
    Interface for keep binding and get Provider

---
+++ src/main/java/by/incubator/infrastructur/Injector.java
@@ -0,0 +1,7 @@
+package by.incubator.infrastructur;
+
+public interface Injector {
+    <T> Provider<T> getProvider(Class<T> type);
+    <T> void bind(Class<T> intf, Class<? extends T> impl);
+    <T> void bindSingleton(Class<T> intf, Class<? extends T> impl);
+}
