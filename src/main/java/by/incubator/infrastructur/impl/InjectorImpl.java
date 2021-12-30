d8558cd32f0d2823855de49fd29ee4442d431fbd
Anton Starasotnikau <anton.starasotnikau@gmail.com>
2021-12-30 13:34:40 +0300

    Write implementation for get Provider and instance
    Write implementation method getProvider (class InjectorImpl) and getInstance (class ProviderImpl)

--- src/main/java/by/incubator/infrastructur/impl/InjectorImpl.java
+++ src/main/java/by/incubator/infrastructur/impl/InjectorImpl.java
@@ -2,18 +2,26 @@ package by.incubator.infrastructur.impl;
 
 import by.incubator.infrastructur.Injector;
 import by.incubator.infrastructur.Provider;
+import by.incubator.infrastructur.annotation.Inject;
+
+import java.lang.reflect.Constructor;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class InjectorImpl implements Injector {
-
+    private final Map<String, Class> bindCache = new HashMap<>();
 
     @Override
     public <T> Provider<T> getProvider(Class<T> type) {
-        return null;
+
+        return new ProviderImpl<>(bindCache.get(type.getSimpleName()));
     }
 
     @Override
     public <T> void bind(Class<T> intf, Class<? extends T> impl) {
-
+        bindCache.put(intf.getSimpleName(), impl);
     }
 
     @Override
--- src/main/java/by/incubator/infrastructur/impl/ProviderImpl.java
+++ src/main/java/by/incubator/infrastructur/impl/ProviderImpl.java
@@ -2,9 +2,24 @@ package by.incubator.infrastructur.impl;
 
 import by.incubator.infrastructur.Provider;
 
+import java.lang.reflect.InvocationTargetException;
+import java.util.List;
+
 public class ProviderImpl<T> implements Provider<T> {
+    private final Class<T> impl;
+
+    public ProviderImpl(Class<T> impl) {
+        this.impl = impl;
+    }
+
     @Override
     public T getInstance() {
-        return null;
+        T obj = null;
+        try {
+            obj = impl.getConstructor().newInstance();
+        } catch (InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
+            e.printStackTrace();
+        }
+        return obj;
     }
 }
