e191006997b6418cd3c782d033b5347277033926
Anton Starasotnikau <anton.starasotnikau@gmail.com>
2021-12-30 16:13:25 +0300

    Create exception for point 4
    Create exception for case when in class declare more than one constructor with @Inject

---
+++ src/main/java/by/incubator/infrastructur/exception/TooManyConstructorsException.java
@@ -0,0 +1,7 @@
+package by.incubator.infrastructur.exception;
+
+public class TooManyConstructorsException extends Throwable {
+    public TooManyConstructorsException(String message) {
+        super(message);
+    }
+}
